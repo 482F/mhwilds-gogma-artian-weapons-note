98a8a793ce3bc3b621f662c203fd8c588480ee77
482F <482F.git.mail@482f.net>
2025-12-20 00:00:00 +0900

    style and use tabs

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,74 @@
 <template>
   <div class="app">
+    <div class="header">mhwilds 巨戟アーティアメモ</div>
+    <tabs
+      class="tabs"
+      :tabs="[
+        { key: 'bonus-note', label: '巨戟復元強化' },
+        { key: 'skill-note', label: 'スキルの再付与' },
+        { key: 'setting', label: '設定' },
+      ]"
+    >
+      <template v-slot:bonus-note>
+        <note />
+      </template>
+      <template v-slot:skill-note>
+        <note />
+      </template>
+      <template v-slot:setting>
+        <setting />
+      </template>
+    </tabs>
   </div>
 </template>
 
 <script setup lang="ts">
 import Note from './feature/note/component.vue'
 import Setting from './feature/setting/component.vue'
+
+import Tabs from './component/tabs/component.vue'
 </script>
 
+<style scoped>
+.app {
+  font-size: 16px;
+  padding: 2% 5%;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+
+  background-color: white;
+
+  display: grid;
+  grid-template-areas:
+    'header'
+    'content';
+  grid-template-rows: max-content 1fr;
+  gap: 32px;
+
+  > .header {
+    grid-area: header;
+    font-size: 32px;
+  }
+  > .content {
+    grid-area: content;
+  }
+}
+</style>
+
 <style>
+html {
+  --c-active: #6c8cff;
+  --c-active-bg: #eef7ff;
+  --c-hover-bg: #fafafa;
+  --c-border: #e3e3e3;
+}
 body {
   margin: 0;
 }
 * {
   font-family: 'Cica';
+  box-sizing: border-box;
   &::-webkit-scrollbar-track {
     background-color: #eee;
   }
