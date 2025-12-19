67fcab24f5c2cb3740c1425e635105147ea94d11
482F <482F.git.mail@482f.net>
2025-12-20 00:00:00 +0900

    features

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app">
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
+<script setup lang="ts">
+import Note from './feature/note/component.vue'
+import Setting from './feature/setting/component.vue'
+</script>
+
+<style>
+body {
+  margin: 0;
+}
 * {
   font-family: 'Cica';
   &::-webkit-scrollbar-track {
---
+++ src/feature/setting/component.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="setting">
+    setting
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
