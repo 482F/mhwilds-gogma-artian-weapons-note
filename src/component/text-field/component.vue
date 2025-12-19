730fd2f999a481969de1e59a0ab42b1ad3551773
482F <482F.git.mail@482f.net>
2025-12-20 00:00:00 +0900

    add text-field

---
+++ src/component/text-field/component.vue
@@ -0,0 +1,18 @@
+<template>
+  <input v-model="value" class="text-field" type="text" :placeholder />
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  placeholder?: string
+}>()
+const value = defineModel<string>()
+</script>
+
+<style scoped>
+.text-field {
+  font-size: 1rem;
+  border: solid 1px var(--c-border);
+  border-radius: 2px;
+}
+</style>
