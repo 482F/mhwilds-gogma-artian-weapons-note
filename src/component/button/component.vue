fc45f2990680201b0b217b26c7301c0f4f4be9ee
482F <482F.git.mail@482f.net>
2025-12-20 00:00:00 +0900

    add button

---
+++ src/component/button/component.vue
@@ -0,0 +1,17 @@
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.button {
+  border: solid 1px var(--c-border);
+  background-color: white;
+  &:hover {
+    background-color: var(--c-hover-bg);
+  }
+}
+</style>
